2a354f556a5e7d01b7c3837c21ef1be295e5a134
christoph <c3#!c4#!123dza>
2018-08-19 18:20:11 +0200

    Beginn der Implementierung des REST Clients. Funktion zur Abfrage der Unternehmensbeschreibungen in Bearbeitung.

---
+++ app/src/main/java/de/gym_kirchseeon/aktienspielboerse/DownloadJsonTask.java
@@ -0,0 +1,48 @@
+package de.gym_kirchseeon.aktienspielboerse;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Don't use this class for your own purposes. Only used by RestClient.
+ */
+
+public class DownloadJsonTask extends AsyncTask<String, Void, JSONObject> {
+
+    @Override
+    protected JSONObject doInBackground(String... strings) {
+        JSONObject result;
+        try {
+            URL url = new URL(strings[0]);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream stream = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder builder = new StringBuilder();
+
+            String inputString;
+            while ((inputString = bufferedReader.readLine()) != null) {
+                builder.append(inputString);
+            }
+
+            result = new JSONObject(builder.toString());
+
+            urlConnection.disconnect();
+
+            return result;
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ app/src/main/java/de/gym_kirchseeon/aktienspielboerse/RestClient.java
@@ -0,0 +1,29 @@
+package de.gym_kirchseeon.aktienspielboerse;
+
+import org.json.JSONObject;
+
+public class RestClient {
+
+
+    public String getCompanyDescription(String company) {
+
+        String urlcompany;
+        JSONObject description;
+
+        if company.contains(" ") {
+            String[] parts;
+            parts = company.split(" ");
+
+            urlcompany = parts[0];
+            for (int i = 1; i < parts.length(); i++) {
+                urlcompany += "%20" + parts[i];
+            }
+
+        }
+
+        //     String url = new URL("https://de.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + urlcompany);
+
+        new DownloadJsonTask().execute("https://de.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + urlcompany);
+
+    }
+}
