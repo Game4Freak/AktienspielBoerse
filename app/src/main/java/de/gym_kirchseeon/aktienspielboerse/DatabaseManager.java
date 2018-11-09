d205945fac60c42976df357ec8e9d35fbc4dc04c
LorenzK <spams.yourcreeps@gmail.com>
2018-11-09 21:27:31 +0100

    Database

--- app/src/main/java/de/gym_kirchseeon/aktienspielboerse/DatabaseManager.java
+++ app/src/main/java/de/gym_kirchseeon/aktienspielboerse/DatabaseManager.java
@@ -1,10 +1,19 @@
 package de.gym_kirchseeon.aktienspielboerse;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseManager extends SQLiteOpenHelper {
+    private AlphaVantageDownloader restClient;
+
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "aktienspielData";
     public static final String TABLE = "companyShares";
@@ -13,22 +22,27 @@ public class DatabaseManager extends SQLiteOpenHelper {
     public static final String KEY_SHORT = "companyShort";
     // Name des Unternehmens
     public static final String KEY_NAME = "companyName";
-    // letzter Wert vor den aktuellen Wert, um Aussage über Kursänderung treffen zu können (Kurs steigt / sinkt)
-    public static final String KEY_SHARES0 = "companyShare0";
-    // aktueller Wert
-    public static final String KEY_SHARES1 = "companyShare1";
+    // letzten 100 werte
+    public static final String[] KEY_SHARES = new String[100];
     // anzahl der gekauften Aktien
     public static final String KEY_AMOUNT = "sharesAmount";
 
     public DatabaseManager(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        for (int i = 0; i < 100; i++){
+            KEY_SHARES[i] = "share" + i;
+        }
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_TABLE = "CREATE TABLE " + TABLE + "("
                 + KEY_ID + " INTEGER PRIMARY KEY," + KEY_SHORT + " TEXT,"
-                + KEY_NAME + " TEXT," + KEY_SHARES0 + " FLOAT," + KEY_SHARES1 + " FLOAT," + KEY_AMOUNT + " INTEGER" + ")";
+                + KEY_NAME + " TEXT,";
+        for (int i = 0; i < KEY_SHARES.length; i++){
+            CREATE_TABLE = CREATE_TABLE + KEY_SHARES[i] + " FLOAT,";
+        }
+        CREATE_TABLE = CREATE_TABLE + KEY_AMOUNT + " INTEGER" + ")";
         db.execSQL(CREATE_TABLE);
 
     }
@@ -38,6 +52,56 @@ public class DatabaseManager extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE);
 
         onCreate(db);
+    }
 
+    public void updateDatabaseAll(){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        List<String> shortList = new ArrayList<>();
+
+        String selectQuery = "SELECT  * FROM " + TABLE;
+
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if(cursor.moveToFirst()){
+            do{
+               shortList.add(cursor.getString(1));
+            } while(cursor.moveToNext());
+        }
+
+        db = this.getWritableDatabase();
+
+        for ( String n: shortList ){
+            JSONObject shares = new JSONObject(); //Restclient getCompany... n
+
+            ContentValues values = new ContentValues();
+            for (int i = 0; i < KEY_SHARES.length; i++){
+                values.put(KEY_SHARES[i], "22.2"); //jeder einzelne eintrag
+            }
+            db.update(TABLE, values, KEY_SHORT + "=?", new String[]{n});
+        }
+    }
+
+    public void updateDatabaseByShort(String companyShort){
+        SQLiteDatabase db = this.getWritableDatabase();
+        JSONObject shares = new JSONObject(); //Restclient getCompany... companyShort
+
+        ContentValues values = new ContentValues();
+        for (int i = 0; i < KEY_SHARES.length; i++){
+            values.put(KEY_SHARES[i], "22.2"); //jeder einzelne eintrag
+        }
+        db.update(TABLE, values, KEY_SHORT + "=?", new String[]{companyShort});
+    }
+
+    public void updateSharesAmountByCompanyShort(String companyShort, int amount){
+
+    }
+
+    public JSONObject getCompanyByCompanyShort(String companyShort){
+        return new JSONObject();
+    }
+
+    public List<JSONObject> getAllCompanies() {
+        return new ArrayList<JSONObject>();
     }
 }
